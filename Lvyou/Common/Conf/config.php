d6d7663015f4f9390c7bc0da1a04c9b70a2da782
linxb <linxb@ename.com>
2016-08-19 18:31:50 +0800

    20160819

--- Lvyou/Common/Conf/config.php
+++ Lvyou/Common/Conf/config.php
@@ -39,10 +39,10 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
+    'DB_HOST'   => '50.93.199.71', // 服务器地址
     'DB_NAME'   => 'lvyou', // 数据库名
-    'DB_USER'   => 'root1', // 用户名
-    'DB_PWD'    => '123456',  // 密码
+    'DB_USER'   => 'lvyou', // 用户名
+    'DB_PWD'    => 'HjHPKKvVNmF8fQL',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'ly_', // 数据库表前缀
 
